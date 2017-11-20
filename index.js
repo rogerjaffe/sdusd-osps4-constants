3b99b1421e4483aaf68c3eaafc54dd988d0da40a
Roger Jaffe <rogerjaffe@gmail.com>
2017-11-20 10:07:11 -0800

    Initial commit; add API server constants

---
+++ index.js
@@ -0,0 +1,13 @@
+const CONSTANTS = {
+  SYSTEM: {
+    DB_PREFIX_PS: 'ps_',
+    DB_PREFIX_OSPS: 'osps_',
+    DB_NAME: 'osps4',
+    EMAIL_LOG_TO: 'rjaffe@sandi.net',
+    EMAIL_HOST: 'mail.sandi.net',
+    LAST_UPDATE_FILE: 'lastUpdate.txt'
+  },
+
+}
+
+module.exports = CONSTANTS

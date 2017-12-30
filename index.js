b8138947e1700bb663a1026627552b8a5381fa38
Roger Jaffe <rogerjaffe@gmail.com>
2017-12-30 07:26:55 -0800

    Change to osps_users_config table

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const CONSTANTS = {
     }
   },
   OSPS_DATA: {
-    OSPS_TABLE_LIST: ['osps_course_lists','osps_users'],
+    OSPS_TABLE_LIST: ['osps_course_lists','osps_users_config'],
     CSV_REQUIREMENT_FLAGS_FILENAME: 'csv_student_data.csv',
     CSV_REQUIREMENT_FLAGS_FIELDS_FILENAME: 'csv_student_data_fields.csv',
     CSV_JSON_FILENAME: 'csv_json.csv',
